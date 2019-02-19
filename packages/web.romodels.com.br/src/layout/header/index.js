8a78b91c3b84d753610835c7e3381458f06e8778
Gustavo Quinta | MoonTory <gustavoqnt40@gmail.com>
2019-02-19 17:05:39 -0300

    fixed: Shifted the nav items to the right side of the screen

--- packages/web.romodels.com.br/src/layout/header/index.js
+++ packages/web.romodels.com.br/src/layout/header/index.js
@@ -1,23 +1,45 @@
 import React, { Fragment } from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 export default function Header(props) {
   return (
     <Fragment>
-      <Navbar bg="light" expand="lg">
-        <LinkContainer to='/' ><Navbar.Brand><img
-        alt=""
-        src="assets/logo_small.svg"
-        width="30"
-        height="30"
-        className="d-inline-block align-top"
-      /></Navbar.Brand></LinkContainer>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <LinkContainer to='/' ><Nav.Link>Home</Nav.Link></LinkContainer>
-            <LinkContainer to='/about' ><Nav.Link>About</Nav.Link></LinkContainer>
+      <Navbar bg='light' expand='lg'>
+        <LinkContainer to='/'>
+          <Navbar.Brand>
+            <img alt='' src='assets/logo_small.svg' width='30' height='30' className='d-inline-block align-top' />
+          </Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav' className='justify-content-end'>
+          <Nav className='justify-content-end'>
+            <LinkContainer to='/'>
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <NavDropdown title='Modelos' id='basic-nav-dropdown'>
+              <LinkContainer to='/modelos-women'>
+                <NavDropdown.Item>Women</NavDropdown.Item>
+              </LinkContainer>
+              <LinkContainer to='/modelos-men'>
+                <NavDropdown.Item>Men</NavDropdown.Item>
+              </LinkContainer>
+              <LinkContainer to='/modelos-kids'>
+                <NavDropdown.Item>Kids</NavDropdown.Item>
+              </LinkContainer>
+            </NavDropdown>
+            <LinkContainer to='/about'>
+              <Nav.Link>Quem Somos</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to='/escola'>
+              <Nav.Link>Escola</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to='/agencia'>
+              <Nav.Link>Servicos</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to='/conact'>
+              <Nav.Link>Quer ser Modelo</Nav.Link>
+            </LinkContainer>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
