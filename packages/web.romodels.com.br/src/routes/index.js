f04b0475fc5af8c455decb98389e161616bcb5e1
Gustavo Quinta | MoonTory <gustavoqnt40@gmail.com>
2019-03-08 16:49:09 -0300

    chore: Added About page to routes

--- packages/web.romodels.com.br/src/routes/index.js
+++ packages/web.romodels.com.br/src/routes/index.js
@@ -1,13 +1,14 @@
 import React, { Fragment } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import { Home } from '../pages';
+import { Home, About } from '../pages';
 
 export function Router() {
   return (
     <Fragment>
       <Switch>
         <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
       </Switch>
     </Fragment>
   );
