9b651d28ab992808a2312dce8d389cd3c4787477
Gustavo Quinta | MoonTory <gustavoqnt40@gmail.com>
2019-03-08 16:49:44 -0300

    chore: Added About Page

---
+++ packages/web.romodels.com.br/src/pages/about/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Jumbotron } from '../../components/Jumbotron';
+import { Team } from '../../components/Team';
+
+export function About() {
+  return (
+    <div>
+      <Jumbotron />
+      <Team />
+    </div>
+  );
+}
