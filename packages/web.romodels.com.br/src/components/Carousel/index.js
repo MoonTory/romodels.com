951ab03a582b3e593ca4b437279a375640becbec
Gustavo Quinta | MoonTory <gustavoqnt40@gmail.com>
2019-03-08 16:58:34 -0300

    fix: Fixed unreadable text in Carousel component

--- packages/web.romodels.com.br/src/components/Carousel/index.js
+++ packages/web.romodels.com.br/src/components/Carousel/index.js
@@ -10,7 +10,7 @@ export default function iCarousel() {
         <Carousel.Item>
           <img className='d-block w-100' src='https://source.unsplash.com/random/1200x800' alt='First slide' />
           <Carousel.Caption>
-            <h3>First slide label</h3>
+            <h3 className='text-white'>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
@@ -18,28 +18,28 @@ export default function iCarousel() {
           <img className='d-block w-100' src='https://source.unsplash.com/random/1200x800' alt='Third slide' />
 
           <Carousel.Caption>
-            <h3>Second slide label</h3>
+            <h3 className='text-white'>Second slide label</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img className='d-block w-100' src='https://source.unsplash.com/random/1200x800' alt='Third slide' />
           <Carousel.Caption>
-            <h3>Third slide label</h3>
+            <h3 className='text-white'>Third slide label</h3>
             <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img className='d-block w-100' src='https://source.unsplash.com/random/1200x800' alt='Third slide' />
           <Carousel.Caption>
-            <h3>Forth slide label</h3>
+            <h3 className='text-white'>Forth slide label</h3>
             <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img className='d-block w-100' src='https://source.unsplash.com/random/1200x800' alt='Third slide' />
           <Carousel.Caption>
-            <h3>Fifth slide label</h3>
+            <h3 className='text-white'>Fifth slide label</h3>
             <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
           </Carousel.Caption>
         </Carousel.Item>
