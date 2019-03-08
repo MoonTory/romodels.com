c6239a253191d0068b92852322acbd9a17fbbf37
Gustavo Quinta | MoonTory <gustavoqnt40@gmail.com>
2019-03-08 16:50:08 -0300

    fix: Fixed main body background color

--- packages/web.romodels.com.br/src/layout/main/index.js
+++ packages/web.romodels.com.br/src/layout/main/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Main({ children }) {
   return (
-    <main role='main' className='bg-light rounded'>
+    <main role='main' className='rounded'>
       {children}
     </main>
   );
