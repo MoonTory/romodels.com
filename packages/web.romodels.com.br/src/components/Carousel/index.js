b4a3373ae6689ccb5d1c5baba9b86c55061bc713
Gustavo Quinta | MoonTory <gustavoqnt40@gmail.com>
2019-03-08 16:46:11 -0300

    chore: Added two more slides to the Carousel compo nent

--- packages/web.romodels.com.br/src/components/Carousel/index.js
+++ packages/web.romodels.com.br/src/components/Carousel/index.js
@@ -29,6 +29,20 @@ export default function iCarousel() {
             <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
           </Carousel.Caption>
         </Carousel.Item>
+        <Carousel.Item>
+          <img className='d-block w-100' src='https://source.unsplash.com/random/1200x800' alt='Third slide' />
+          <Carousel.Caption>
+            <h3>Forth slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className='d-block w-100' src='https://source.unsplash.com/random/1200x800' alt='Third slide' />
+          <Carousel.Caption>
+            <h3>Fifth slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
       </Carousel>
     </Fragment>
   );
