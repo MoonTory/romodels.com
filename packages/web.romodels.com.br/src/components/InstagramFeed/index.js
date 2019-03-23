ddfc71b685bbb427c85d5e9b18ab418d5d2ec860
MoonTory <gustavoqnt40@gmail.com>
2019-03-23 17:48:29 -0300

    fix: Fixed config for important info

--- packages/web.romodels.com.br/src/components/InstagramFeed/index.js
+++ packages/web.romodels.com.br/src/components/InstagramFeed/index.js
@@ -5,6 +5,8 @@ import { Spinner } from '../Spinner';
 
 import './style/index.scss';
 
+import config from '../../config';
+
 export class InstagramFeed extends Component {
   state = {
     data: [],
@@ -13,7 +15,9 @@ export class InstagramFeed extends Component {
 
   async componentDidMount() {
     const instaData = await axios.get(
-      'https://api.instagram.com/v1/users/self/media/recent/?access_token=3597322204.fc05a75.95d7898c38cb4b70922dcdb9005ff665&count=8'
+      `https://api.instagram.com/v1/users/self/media/recent/?access_token=${
+        config.IG_ACCESS_TOKEN
+      }&count=8`
     );
 
     this.setState({ data: instaData.data.data, isFetching: false });
@@ -22,27 +26,53 @@ export class InstagramFeed extends Component {
   render() {
     return (
       <section>
-        <div style={{ paddingBottom: '4rem', paddingTop: '4rem' }} className='container instagram'>
+        <div
+          style={{ paddingBottom: '4rem', paddingTop: '4rem' }}
+          className='container instagram'
+        >
           <div style={{ marginBottom: '2rem' }} className='row'>
             <div className='col-lg-12'>
               <h3 className='font-weight-bold'>Novidades do Instagram!</h3>
               <hr />
             </div>
             {this.state.isFetching ? (
-              <div style={{ paddingTop: '2rem' }} className='container text-center insta-load'>
+              <div
+                style={{ paddingTop: '2rem' }}
+                className='container text-center insta-load'
+              >
                 <Spinner />
               </div>
             ) : (
               this.state.data.map((value, index) => {
                 return (
-                  <div key={index} style={{ marginBottom: '1rem', marginTop: '1rem' }} className='col-md-3'>
+                  <div
+                    key={index}
+                    style={{ marginBottom: '1rem', marginTop: '1rem' }}
+                    className='col-md-3'
+                  >
                     <a href={value.link}>
                       <div className='card shadow-sm'>
-                        <img src={value.images.low_resolution.url} alt={index} className='card-img-top' />
-                        <div style={{ padding: '0.35rem' }} className='card-body'>
-                          <i style={{ color: 'red', paddingRight: '0.5rem' }} className='fas fa-heart' />
+                        <img
+                          src={value.images.low_resolution.url}
+                          alt={index}
+                          className='card-img-top'
+                        />
+                        <div
+                          style={{ padding: '0.35rem' }}
+                          className='card-body'
+                        >
+                          <i
+                            style={{ color: 'red', paddingRight: '0.5rem' }}
+                            className='fas fa-heart'
+                          />
                           {value.likes.count}
-                          <i style={{ paddingLeft: '0.5rem', paddingRight: '0.5rem' }} className='fas fa-comments' />
+                          <i
+                            style={{
+                              paddingLeft: '0.5rem',
+                              paddingRight: '0.5rem'
+                            }}
+                            className='fas fa-comments'
+                          />
                           {value.comments.count}
                         </div>
                       </div>
