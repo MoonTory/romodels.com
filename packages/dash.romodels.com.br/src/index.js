15b919832dd5c44bab3e0b3dfaf246b27a04a22d
Gustavo Quinta | MoonTory <gustavoqnt40@gmail.com>
2019-04-05 13:34:34 -0300

    minor: Redux boilerplate
    - Added the redux boilerplate so that the app can manage the state
     through redux.

--- packages/dash.romodels.com.br/src/index.js
+++ packages/dash.romodels.com.br/src/index.js
@@ -2,15 +2,19 @@ import '@babel/polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import { App } from './app';
+import { store } from './lib/redux/store';
 
 import 'bootswatch/dist/lux/bootstrap.min.css';
-import './style/tsuki_strap.css';
+import './assets/styles/tsuki_strap.css';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
